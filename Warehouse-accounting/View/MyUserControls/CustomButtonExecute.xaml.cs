53ff950208925c60348ddfbc7a6531a722f6648d
qpashkaaa <qpashkaaa@gmail.com>
2023-07-03 23:47:43 +0300

    add authorization view

---
+++ Warehouse-accounting/View/MyUserControls/CustomButtonExecute.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Warehouse_accounting.View.MyUserControls
+{
+    /// <summary>
+    /// Логика взаимодействия для CustomButtonExecute.xaml
+    /// </summary>
+    public partial class CustomButtonExecute : UserControl
+    {
+        public CustomButtonExecute()
+        {
+            InitializeComponent();
+        }
+        private string placeholder;
+        public string Placeholder
+        {
+            get { return placeholder; }
+            set
+            {
+                placeholder = value;
+                btnPlaceholder.Text = placeholder;
+            }
+        }
+
+        private System.Windows.Media.Brush color;
+
+        public System.Windows.Media.Brush Color
+        {
+            get { return color; }
+            set
+            {
+                color = value;
+                btnStyle.Background = color;
+            }
+        }
+    }
+}
---
+++ Warehouse-accounting/View/MyUserControls/CustomToolBar.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Warehouse_accounting.View.MyUserControls
+{
+    /// <summary>
+    /// Логика взаимодействия для CustomToolBar.xaml
+    /// </summary>
+    public partial class CustomToolBar : UserControl
+    {
+        public CustomToolBar()
+        {
+            InitializeComponent();
+        }
+    }
+}
