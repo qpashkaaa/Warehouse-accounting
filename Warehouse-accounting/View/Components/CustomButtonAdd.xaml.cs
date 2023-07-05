756ed4ef3bee1576682533f64e4c450e9d70c450
qpashkaaa <qpashkaaa@gmail.com>
2023-07-05 14:24:38 +0300

    Add UI elements

---
+++ Warehouse-accounting/View/Components/CustomButtonAdd.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Warehouse_accounting.View.Components
+{
+    /// <summary>
+    /// Логика взаимодействия для CustomButtonAdd.xaml
+    /// </summary>
+    public partial class CustomButtonAdd : UserControl
+    {
+        public CustomButtonAdd()
+        {
+            InitializeComponent();
+        }
+
+        private string placeholder;
+        public string Placeholder
+        {
+            get { return placeholder; }
+            set
+            {
+                placeholder = value;
+                btnPlaceholder.Text = placeholder;
+            }
+        }
+    }
+}
---
+++ Warehouse-accounting/View/Components/CustomTextBoxDataGridSearch.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Warehouse_accounting.View.Components
+{
+    /// <summary>
+    /// Логика взаимодействия для CustomTextBoxDataGridSearch.xaml
+    /// </summary>
+    public partial class CustomTextBoxDataGridSearch : UserControl
+    {
+        public CustomTextBoxDataGridSearch()
+        {
+            InitializeComponent();
+        }
+
+        private string placeholder;
+
+        public string Placeholder
+        {
+            get { return placeholder; }
+            set
+            {
+                placeholder = value;
+                tbPlaceholder.Text = placeholder;
+            }
+        }
+
+        private void txtInput_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtInput.Text))
+                tbPlaceholder.Visibility = Visibility.Visible;
+            else
+                tbPlaceholder.Visibility = Visibility.Hidden;
+        }
+    }
+}
