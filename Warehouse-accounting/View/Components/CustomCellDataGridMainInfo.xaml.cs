42195084bca165c1170c5d8e8e77a3d4d3ebe583
qpashkaaa <qpashkaaa@gmail.com>
2023-07-06 14:35:55 +0300

    add UI component

---
+++ Warehouse-accounting/View/Components/CustomCellDataGridMainInfo.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Warehouse_accounting.View.Components
+{
+    /// <summary>
+    /// Логика взаимодействия для CustomCellDataGridMainInfo.xaml
+    /// </summary>
+    public partial class CustomCellDataGridMainInfo : UserControl
+    {
+        public CustomCellDataGridMainInfo()
+        {
+            InitializeComponent();
+        }
+
+        private string placeholder;
+        public string Placeholder
+        {
+            get { return placeholder; }
+            set
+            {
+                placeholder = value;
+                cellPlaceholder.Text = placeholder;
+                string iconShortName;
+                string[] name = placeholder.Split(' ');
+                if (name.Length > 1)
+                {
+                    iconShortName = name[0][0].ToString().ToUpper() + name[1][0].ToString().ToUpper();
+                }
+                else
+                {
+                    iconShortName = name[0][0].ToString().ToUpper();
+                }
+                elemIconShortName.Placeholder = iconShortName;
+            }
+        }
+    }
+}
---
+++ Warehouse-accounting/View/Components/CustomCellDataGridTextInfo.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Warehouse_accounting.View.Components
+{
+    /// <summary>
+    /// Логика взаимодействия для CustomCellDataGridTextInfo.xaml
+    /// </summary>
+    public partial class CustomCellDataGridTextInfo : UserControl
+    {
+        public CustomCellDataGridTextInfo()
+        {
+            InitializeComponent();
+        }
+
+        private string placeholder;
+        public string Placeholder
+        {
+            get { return placeholder; }
+            set
+            {
+                placeholder = value;
+                mainPlaceholder.Text = placeholder;
+            }
+        }
+
+        private string optPlaceholder;
+        public string OptPlaceholder
+        {
+            get { return optPlaceholder; }
+            set
+            {
+                optPlaceholder = value;
+                optionalPlaceholder.Text = optPlaceholder;
+            }
+        }
+    }
+}
