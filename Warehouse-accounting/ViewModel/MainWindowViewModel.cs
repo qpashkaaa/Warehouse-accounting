aaa0b565532f733d945efad449ac8fa7bd0960cd
qpashkaaa <qpashkaaa@gmail.com>
2023-07-16 23:26:54 +0300

    deleted

--- Warehouse-accounting/ViewModel/MainWindowViewModel.cs
+++ Warehouse-accounting/ViewModel/MainWindowViewModel.cs
@@ -1,15 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
+﻿using System.Windows.Controls;
 using System.Windows.Input;
-using Warehouse_accounting.View;
 using Warehouse_accounting.View.Components;
-using System.Diagnostics;
 using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.Command;
 using Warehouse_accounting.Storage.ViewModels;
